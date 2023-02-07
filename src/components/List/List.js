35a518ceef31348d8b7702c88de4416386e947b5
Karina Penkala <marsz@poczta.onet.pl>
2023-02-07 13:58:40 +0100

    change import Column and SearchFrom

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,14 @@
 import styles from './List.module.scss';
-import Column from '../Column/Column';
+import Column from '../../Column/Column';
+import SearchForm from '../../SearchForm/SearchForm';
 
 const List = () => 
     <body className={styles.list}>
-    <header className={styles.header}>
-      <h2 className={styles.title}>Things to do <span>soon</span></h2>
+        <header className={styles.header}>
+            <h2 className={styles.title}>Things to do <span>soon</span></h2>
             <p className={styles.description}>Interseting things I want to check out</p>
         </header>
+        <SearchForm />
         <div className={styles.columns}>
             <Column title="Books" />
             <Column title="Movies" />
