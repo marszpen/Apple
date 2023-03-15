a6a35b283c918069c0a8882568ab33bcf7b09ad1
Karina Penkala <marsz@poczta.onet.pl>
2023-03-15 23:41:00 +0100

    add Favorite and About

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+import style from './About.module.scss';
+
+const About = () => {
+    <div>
+        <h1 className={style.about}>ABOUT</h1>
+        <p>Lorem Ipsum</p>
+    </div>
+}
+
+export default About;
---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,10 @@
+import style from './Favorite.module.scss';
+
+const Favorite = () => {
+    <div>
+        <h1 className={style.favorite}>FAVORITE</h1>
+        <p>Lorem Ipsum</p>
+    </div>
+}
+
+export default Favorite;
