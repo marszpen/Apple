edd85a04095830c3a8831a43d45ca6101e87ea39
Karina Penkala <marsz@poczta.onet.pl>
2023-04-03 23:40:54 +0200

    add REMOVE_CARD

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleCardFavorite } from '../../redux/cardsReducer';
+import { removeCard } from "../../redux/cardsReducer";
 import styles from './Card.module.scss';
 import clsx from 'clsx'
 
@@ -15,13 +16,26 @@ const Card = props => {
         console.log(cardId);
         dispatch(toggleCardFavorite(cardId));
     }
+
+    const deleteCard = e => {
+        e.preventDefault();
+        dispatch(removeCard(cardId));
+    }
     
     
     return (
         <li className={styles.card}>{props.title}
-        <button onClick={handleSubmit} className={clsx(styles.button, favoriteValue && styles.isActive)}>
-            <span className={"fa fa-star-o"}></span>
-        </button>
+            <div className={styles.buttons}>
+                <button onClick={handleSubmit}
+                    className={clsx(styles.button, favoriteValue && styles.isActive)}>
+                    <span className={"fa fa-star-o"}></span>
+                </button>
+                <button onClick={deleteCard}
+                    className={styles.button}
+                >
+                    <i className={'fa fa-trash'} />
+                </button>
+            </div>
         </li>
     );
 };
